6120c5a6beb6e56b99060873646236ebe166e970
abkn <abkn0905@gmail.com>
2018-07-19 18:32:55 +0900

    Initial commit

---
+++ Assets/AirTapGesture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using HoloToolkit.Unity.InputModule;
+
+public class AirTapGesture : MonoBehaviour, IInputClickHandler
+{
+    public GameObject original;
+
+    // Use this for initialization
+    void Start()
+    {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+       // gameObject.AddComponent<Rigidbody>();
+
+        GameObject cube = GameObject.Instantiate(original);
+        cube.transform.position = Camera.main.transform.TransformPoint(0, 0, 1.2f);
+    }
+}
---
+++ Assets/GazeInputTarget.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+using VRStandardAssets.Utils;
+
+public class GazeInputTarget : MonoBehaviour, IFocusable
+{
+
+    public GameObject Explosion;
+    // public AudioSource AS;
+    public AudioClip sound01;
+    public AudioClip sound02;
+
+    public void OnFocusEnter()
+    {
+        //オブジェクトに視線が当たったらゲージがいっぱいになった時のイベントを登録
+        SelectionRadial.Instance.OnSelectionComplete += OnSelectionComplete;
+        SelectionRadial.Instance.Show();
+        SelectionRadial.Instance.HandleDown();
+        GetComponent<AudioSource>().PlayOneShot(sound02);
+    }
+
+    public void OnFocusExit()
+    {
+        //オブジェクトに視線がはずれたらゲージがいっぱいになった時のイベントを解除
+        SelectionRadial.Instance.OnSelectionComplete -= OnSelectionComplete;
+        SelectionRadial.Instance.HandleUp();
+        SelectionRadial.Instance.Hide();
+
+    }
+
+    private void OnSelectionComplete()
+    {
+        //ここにゲージがいっぱいになったら実行される処理を書く。今回は色を青に変更
+        //gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
+        AudioSource.PlayClipAtPoint(sound01, transform.position);
+        Destroy(this.gameObject);
+
+        Instantiate(Explosion, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+
+        SelectionRadial.Instance.OnSelectionComplete -= OnSelectionComplete;
+        SelectionRadial.Instance.HandleUp();
+        SelectionRadial.Instance.Hide();
+    }
+}
