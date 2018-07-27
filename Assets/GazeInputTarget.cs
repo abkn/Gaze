bf06057cd30461ecb976d87fc96c692813f201fd
abkn <abkn0905@gmail.com>
2018-07-27 11:29:55 +0900

    Gaze

--- Assets/GazeInputTarget.cs
+++ Assets/GazeInputTarget.cs
@@ -1,21 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using HoloToolkit.Unity.InputModule;
-using System;
 using VRStandardAssets.Utils;
 
 public class GazeInputTarget : MonoBehaviour, IFocusable
 {
 
     public GameObject Explosion;
-    // public AudioSource AS;
     public AudioClip sound01;
     public AudioClip sound02;
 
     public void OnFocusEnter()
     {
-        //オブジェクトに視線が当たったらゲージがいっぱいになった時のイベントを登録
         SelectionRadial.Instance.OnSelectionComplete += OnSelectionComplete;
         SelectionRadial.Instance.Show();
         SelectionRadial.Instance.HandleDown();
@@ -24,7 +19,6 @@ public class GazeInputTarget : MonoBehaviour, IFocusable
 
     public void OnFocusExit()
     {
-        //オブジェクトに視線がはずれたらゲージがいっぱいになった時のイベントを解除
         SelectionRadial.Instance.OnSelectionComplete -= OnSelectionComplete;
         SelectionRadial.Instance.HandleUp();
         SelectionRadial.Instance.Hide();
@@ -33,9 +27,6 @@ public class GazeInputTarget : MonoBehaviour, IFocusable
 
     private void OnSelectionComplete()
     {
-        //ここにゲージがいっぱいになったら実行される処理を書く。今回は色を青に変更
-        //gameObject.GetComponent<Renderer>().material.color = Color.blue;
-
         AudioSource.PlayClipAtPoint(sound01, transform.position);
         Destroy(this.gameObject);
 
